13a21a2bdcfd3627cd13d5732e0716c85505b325
topilson <139675483+topilson@users.noreply.github.com>
2023-07-22 23:49:53 +0100

    Create Scripts.js

---
+++ Scripts.js
@@ -0,0 +1,8 @@
+// Coloque aqui qualquer função ou interatividade que você queira adicionar ao site.
+// Por exemplo, um formulário de contato com validação ou animações.
+
+// Exemplo de função para exibir uma mensagem ao enviar o formulário de contato
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Obrigado por entrar em contato! Em breve responderemos sua mensagem.');
+});
