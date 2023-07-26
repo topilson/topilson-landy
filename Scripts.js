1042c4615b707c6aa7e3dfdffc02fa4e05d2e42e
topilson <139675483+topilson@users.noreply.github.com>
2023-07-26 22:01:07 +0100

    Atualizar o Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -1,19 +1,39 @@
-// Função para exibir o endereço IP no parágrafo com id "ipAddress"
-function displayIPAddress(ip) {
-  const ipAddressElement = document.getElementById("ipAddress");
-  ipAddressElement.textContent = ip;
-}
+const express = require('express');
+const app = express();
+const port = 3000;
 
-// Função para obter o endereço IP do cliente
-function getIPAddress() {
-  fetch("https://api.ipify.org?format=json")
-    .then((response) => response.json())
-    .then((data) => {
-      const ipAddress = data.ip;
-      displayIPAddress(ipAddress);
-    })
-    .catch((error) => console.error("Erro ao obter endereço IP:", error));
-}
+// Armazenar os endereços IP dos dispositivos que acessam o link
+const ipAddresses = [];
 
-// Chamada da função para obter o endereço IP quando a página é carregada
-getIPAddress();
+// Middleware para obter o endereço IP do cliente e armazená-lo
+app.use((req, res, next) => {
+  const ipAddress = req.ip;
+  ipAddresses.push(ipAddress);
+  console.log('Novo dispositivo acessando o link. Endereço IP:', ipAddress);
+  next();
+});
+
+// Rota para exibir a lista de endereços IP rastreados
+app.get('/', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Endereços IP Rastreados</title>
+    </head>
+    <body>
+      <h1>Endereços IP Rastreados:</h1>
+      <ul>
+        ${ipAddresses.map(ip => `<li>${ip}</li>`).join('')}
+      </ul>
+    </body>
+    </html>
+  `);
+});
+
+// Iniciar o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
