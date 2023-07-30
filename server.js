7b3961ad107e43f7a16814593ff4351ceb1fc07a
topilson <139675483+topilson@users.noreply.github.com>
2023-07-30 13:01:10 +0100

    Atualizar o server.js

--- server.js
+++ server.js
@@ -1,39 +1,77 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Formulário de Cadastro e Login</title>
+  <style>
+    body {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      margin: 0;
+    }
 
-// Armazenar os endereços IP dos dispositivos que acessam o link
-const ipAddresses = [];
+    .container {
+      border: 1px solid #ccc;
+      padding: 20px;
+      margin: 10px;
+      width: 300px;
+      text-align: center;
+    }
 
-// Middleware para obter o endereço IP do cliente e armazená-lo
-app.use((req, res, next) => {
-  const ipAddress = req.ip;
-  ipAddresses.push(ipAddress);
-  console.log('Novo dispositivo acessando o link. Endereço IP:', ipAddress);
-  next();
-});
+    button {
+      margin-top: 10px;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <h1>Cadastro</h1>
+    <input type="text" id="numero" placeholder="Número">
+    <input type="text" id="usuario" placeholder="Nome de Usuário">
+    <input type="password" id="senha" placeholder="Senha">
+    <button onclick="cadastrar()">Cadastrar</button>
+  </div>
 
-// Rota para exibir a lista de endereços IP rastreados
-app.get('/', (req, res) => {
-  res.send(`
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Endereços IP Rastreados</title>
-    </head>
-    <body>
-      <h1>Endereços IP Rastreados:</h1>
-      <ul>
-        ${ipAddresses.map(ip => `<li>${ip}</li>`).join('')}
-      </ul>
-    </body>
-    </html>
-  `);
-});
+  <div class="container">
+    <h1>Login</h1>
+    <input type="text" id="numeroLogin" placeholder="Número">
+    <input type="password" id="senhaLogin" placeholder="Senha">
+    <button onclick="login()">Login</button>
+  </div>
 
-// Iniciar o servidor
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
-});
+  <script>
+    const cadastro = {
+      numero: "956907156",
+      senha: "12345top",
+      usuario: "usuario123"
+    };
+
+    function cadastrar() {
+      const numero = document.getElementById("numero").value;
+      const usuario = document.getElementById("usuario").value;
+      const senha = document.getElementById("senha").value;
+
+      if (numero === cadastro.numero && senha === cadastro.senha) {
+        alert("Usuário já cadastrado!");
+      } else {
+        cadastro.numero = numero;
+        cadastro.senha = senha;
+        cadastro.usuario = usuario;
+        alert("Cadastro realizado com sucesso!");
+      }
+    }
+
+    function login() {
+      const numeroLogin = document.getElementById("numeroLogin").value;
+      const senhaLogin = document.getElementById("senhaLogin").value;
+
+      if (numeroLogin === cadastro.numero && senhaLogin === cadastro.senha) {
+        alert("Login realizado com sucesso! Bem-vindo, " + cadastro.usuario + "!");
+      } else {
+        alert("Número ou senha incorretos. Tente novamente!");
+      }
+    }
+  </script>
+</body>
+</html>
